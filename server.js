9a91cd4f5a48f9b8c1490a72b676829af9b23ea9
unknown <rahulkale360@gmail.com>
2020-08-30 16:46:18 +0530

    changed path

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ app.use(express.static(__dirname + '/dist/spotifyclone'));
 
 //PathLocationStrategy
 app.get('/*',function(req, res){
-    res.sendFile(path.join(__dirname + '/dist/index.html'))
+    res.sendFile(path.join(__dirname + '/dist/spotifyclone/index.html'))
 })
 
 app.listen(process.env.PORT || 8080);
