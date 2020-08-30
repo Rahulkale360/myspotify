919d97ce5decee5f8b9e7a7f05875ab3471c0802
unknown <rahulkale360@gmail.com>
2020-08-30 15:25:52 +0530

    updated enviroment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,15 @@
 export const environment = {
-  production: true
+  production: true,
+    spotifySecret: 'b94e7c4937f7405283070ee56b44a479',
+    authUrl : "https://accounts.spotify.com/authorize",
+    redirectUrl : "http://musicspotify.herokuapp.com/home",
+    clientId : "48f4fa8b23be472a9a8de596aa8344b1",
+    scopes : [
+      "user-read-currently-playing",
+      "user-read-currently-played",
+      "user-read-playback-state",
+      "user-top-read",
+      "user-modify-playback-state",
+    ]
+  
 };
