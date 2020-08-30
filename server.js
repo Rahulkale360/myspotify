cbee7dffc07449c17fa0897dd3dacbcd7765becb
unknown <rahulkale360@gmail.com>
2020-08-30 15:36:30 +0530

    update server.js

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist/spotifyclone'));
 
 //PathLocationStrategy
 app.get('/*',function(req, res){
-    res.sendFile(path.join(__dirname + '/dist/index.html'))
+    res.sendFile(path.join(__dirname + '/dist/spotifyclone/index.html'))
 })
 
 app.listen(process.env.PORT || 8080);
+console.log("server started");
